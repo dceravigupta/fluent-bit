1f2f1ea3a13680ac732fbb7c85361a3e8f12e177
Ravi Gupta <dceravigupta@gmail.com>
2025-05-06 11:41:59 -0700

    1

--- plugins/out_azure_kusto/azure_kusto.c
+++ plugins/out_azure_kusto/azure_kusto.c
@@ -1120,11 +1120,9 @@ static int azure_kusto_traces_format(struct flb_azure_kusto *ctx, const char *ta
 
                         msgpack_pack_str(&mp_pck, 8);
                         msgpack_pack_str_body(&mp_pck, "resource", 8);
-                        //msgpack_pack_str(&mp_pck, flb_sds_len(ctx->tag_key));
-                        //msgpack_pack_str_body(&mp_pck, ctx->tag_key, flb_sds_len(ctx->tag_key));
                         msgpack_pack_object(&mp_pck, *resource);
                         
-                        msgpack_pack_str(&mp_pck, flb_sds_len(ctx->tag_key));
+                        /*msgpack_pack_str(&mp_pck, flb_sds_len(ctx->tag_key));
                         msgpack_pack_str_body(&mp_pck, ctx->tag_key, flb_sds_len(ctx->tag_key));
                         msgpack_pack_object(&mp_pck, *schema_url);
 
@@ -1134,25 +1132,19 @@ static int azure_kusto_traces_format(struct flb_azure_kusto *ctx, const char *ta
 
                         msgpack_pack_str(&mp_pck, flb_sds_len(ctx->tag_key));
                         msgpack_pack_str_body(&mp_pck, ctx->tag_key, flb_sds_len(ctx->tag_key));
+                        msgpack_pack_object(&mp_pck, *span);*/
+
+                        msgpack_pack_str(&mp_pck, 10);
+                        msgpack_pack_str_body(&mp_pck, "schema_url", 10);
+                        msgpack_pack_object(&mp_pck, *schema_url);
+
+                        msgpack_pack_str(&mp_pck, 5);
+                        msgpack_pack_str_body(&mp_pck, "scope", 5);
+                        msgpack_pack_object(&mp_pck, *scope);
+
+                        msgpack_pack_str(&mp_pck, 4);
+                        msgpack_pack_str_body(&mp_pck, "span", 4);
                         msgpack_pack_object(&mp_pck, *span);
-
-                        //msgpack_pack_str(&mp_pck, flb_sds_len("resource"));
-                        //msgpack_pack_str_body(&mp_pck, "resource", flb_sds_len("resource"));
-                        //msgpack_pack_object(&mp_pck, *resource);
-
-                        //msgpack_pack_str(&mp_pck, flb_sds_len("schema_url"));
-                        //msgpack_pack_str_body(&mp_pck, "schema_url", flb_sds_len("schema_url"));
-                        //msgpack_pack_object(&mp_pck, *schema_url);
-
-                        //msgpack_pack_str(&mp_pck, flb_sds_len("scope"));
-                        //msgpack_pack_str_body(&mp_pck, "scope", flb_sds_len("scope"));
-                        //msgpack_pack_object(&mp_pck, *scope);
-
-                        //msgpack_pack_str(&mp_pck, flb_sds_len("span"));
-                        //msgpack_pack_str_body(&mp_pck, "span", flb_sds_len("span"));
-                        //msgpack_pack_str(&mp_pck, flb_sds_len("span"));
-                        //msgpack_pack_str_body(&mp_pck, "span", flb_sds_len("span"));
-                        //msgpack_pack_object(&mp_pck, *span);
 
                         flb_sds_t json_record = flb_msgpack_raw_to_json_sds(mp_sbuf.data, mp_sbuf.size);
                         if (!json_record){
