5be2b61ed2cbcc44103d38b399533713e1fac7d4
Ravi Gupta <dceravigupta@gmail.com>
2025-05-06 11:33:26 -0700

    1

--- plugins/out_azure_kusto/azure_kusto.c
+++ plugins/out_azure_kusto/azure_kusto.c
@@ -1114,6 +1114,7 @@ static int azure_kusto_traces_format(struct flb_azure_kusto *ctx, const char *ta
                             msgpack_pack_str_body(&mp_pck, tag, tag_len);
                         }
                         
+                        
                         msgpack_pack_str(&mp_pck, flb_sds_len(ctx->log_key));
                         msgpack_pack_str_body(&mp_pck, ctx->log_key, flb_sds_len(ctx->log_key));
                         msgpack_pack_map(&mp_pck, 4);
