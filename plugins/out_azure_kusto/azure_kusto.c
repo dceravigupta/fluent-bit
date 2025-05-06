22b4ddaabcaf0f2b49d52e4c16772f51f412b6b4
Ravi Gupta <dceravigupta@gmail.com>
2025-05-06 11:23:22 -0700

    1

--- plugins/out_azure_kusto/azure_kusto.c
+++ plugins/out_azure_kusto/azure_kusto.c
@@ -1116,7 +1116,8 @@ static int azure_kusto_traces_format(struct flb_azure_kusto *ctx, const char *ta
                         
                         msgpack_pack_str(&mp_pck, flb_sds_len(ctx->log_key));
                         msgpack_pack_str_body(&mp_pck, ctx->log_key, flb_sds_len(ctx->log_key));
-                        msgpack_pack_map(&mp_pck, 1);
+                        msgpack_pack_object(&mp_pck, *span);
+                        //msgpack_pack_map(&mp_pck, 1);
                         
                         //msgpack_pack_str(&mp_pck, flb_sds_len("resource"));
                         //msgpack_pack_str_body(&mp_pck, "resource", flb_sds_len("resource"));
@@ -1130,10 +1131,10 @@ static int azure_kusto_traces_format(struct flb_azure_kusto *ctx, const char *ta
                         //msgpack_pack_str_body(&mp_pck, "scope", flb_sds_len("scope"));
                         //msgpack_pack_object(&mp_pck, *scope);
 
-                        msgpack_pack_str(&mp_pck, flb_sds_len("span"));
-                        msgpack_pack_str_body(&mp_pck, "span", flb_sds_len("span"));
-                        msgpack_pack_str(&mp_pck, flb_sds_len("span"));
-                        msgpack_pack_str_body(&mp_pck, "span", flb_sds_len("span"));
+                        //msgpack_pack_str(&mp_pck, flb_sds_len("span"));
+                        //msgpack_pack_str_body(&mp_pck, "span", flb_sds_len("span"));
+                        //msgpack_pack_str(&mp_pck, flb_sds_len("span"));
+                        //msgpack_pack_str_body(&mp_pck, "span", flb_sds_len("span"));
                         //msgpack_pack_object(&mp_pck, *span);
 
                         flb_sds_t json_record = flb_msgpack_raw_to_json_sds(mp_sbuf.data, mp_sbuf.size);
